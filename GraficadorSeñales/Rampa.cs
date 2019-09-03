456a13caea81cf1bd3a0cbfd04c78da042eb5e98
sofiabelmar <sofiabelmar@users.noreply.github.com>
2019-09-03 10:27:36 -0700

    Add project files.

---
+++ GraficadorSeñales/Rampa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class Rampa
+    {
+        public List<Muestra> Muestras
+        { get; set; }
+
+        public Rampa()
+        {
+            Muestras =
+                new List<Muestra>();
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado;
+
+            if (tiempo >= 0)
+            {
+                resultado = tiempo;
+            }
+            else
+            {
+                resultado = 0.0;
+            }
+
+            return resultado;
+             }
+        }
+}
